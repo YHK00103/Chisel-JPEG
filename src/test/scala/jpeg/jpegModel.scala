ebe86c310b16e7ccce65fa7075dd2df07d5bd09f
Darren Lin <dlin52@ucsc.edu>
2024-02-20 16:47:04 -0800

    basic funcs

--- src/test/scala/jpeg/jpegModel.scala
+++ src/test/scala/jpeg/jpegModel.scala
@@ -1,1 +1,13 @@
+package jpeg
 
+class jpegEncode(decompress: Boolean, quantTable: List[List[int]], encoding: Int){
+    
+    def zigzagParse(table: List[List[Int]]): Unit = {
+
+    }
+
+    def DCT(): Unit = {
+
+    }
+
+}
