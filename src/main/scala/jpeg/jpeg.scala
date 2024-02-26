355c613ff412d95fdb92385cf84fe3bc7e5acf04
johndrab <john.drab@yahoo.com>
2024-02-26 01:14:10 -0800

    setting up Chisel DCT

--- src/main/scala/jpeg/jpeg.scala
+++ src/main/scala/jpeg/jpeg.scala
@@ -8,3 +8,12 @@ case class jpegParams() {
     val rowLen: Int = 8
     val colLen: Int = 8
 }
+
+class jpegEncodeChisel extends Module {
+  val io = IO(new Bundle {
+    val pixelMatrixIn = Input(Vec(8, Vec(8, SInt(8.W))))
+    val dctOut = Output(Vec(8, Vec(8, FixedPoint(16.W, 8.BP))))
+  })
+
+
+}
