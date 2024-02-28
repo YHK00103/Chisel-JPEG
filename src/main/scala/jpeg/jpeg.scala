a891f97dd963e86d0f6010aaad398286a1c1187e
Darren Lin <dlin52@ucsc.edu>
2024-02-28 15:16:00 -0800

    separating all modules

--- src/main/scala/jpeg/jpeg.scala
+++ src/main/scala/jpeg/jpeg.scala
@@ -1,39 +1,13 @@
-// package jpeg
+package jpeg
 
-// import chisel3._
-// import chisel3.internal.firrtl.Width
-// import chisel3.util._
+import chisel3._
+import chisel3.internal.firrtl.Width
+import chisel3.util._
 
-// case class jpegParams() {
-//     val rowLen: Int = 8
-//     val colLen: Int = 8
-// }
+case class jpegParams() {
+    val rowLen: Int = 8
+    val colLen: Int = 8
+}
 
-// class jpegEncodeChisel extends Module {
-//     val io = IO(new Bundle {
-//         val matrixIn = Input(Vec(8, Vec(8, SInt(8.W))))
-//         val dctOut = Output(Vec(8, Vec(8, FixedPoint(16.W, 8.BP))))
-//     })
-
-//     def DCT(matrix: Vec[Vec[SInt]], width: Int, height: Int): Vec[Vec[FixedPoint]] = {
-//         val dctMatrix = Wire(Vec(8, Vec(8, FixedPoint(16.W, 8.BP))))
-
-//         for (u <- 0 until 8) {
-//             for (v <- 0 until 8) {
-//                 val sum = (0 until 8).foldLeft(0.0.F(16.BP)) { (accI, i) =>
-//                     (0 until 8).foldLeft(accI) { (accJ, j) =>
-//                         val pixelValue = matrix(i)(j).asFixedPoint(8.BP)
-//                         val tempSum = accJ + pixelValue * cos((2 * i + 1) * u * Pi.F(16.BP) / 16) * cos((2 * j + 1) * v * Pi.F(16.BP) / 16)
-//                         tempSum
-//                     }
-//                 }
-//                 val alphaU = if (u == 0) 1.0.F(16.BP) else math.sqrt(2).F(16.BP) / 2.0.F(16.BP)
-//                 val alphaV = if (v == 0) 1.0.F(16.BP) else math.sqrt(2).F(16.BP) / 2.0.F(16.BP)
-//                 dctMatrix(u)(v) := (alphaU * alphaV * sum / 4.0.F(16.BP)).toDouble
-//             }
-//         }
-//         dctMatrix
-//     }
-
-//     io.dctOut := DCT(io.matrixIn, 8, 8)
-// }
+class jpegEncodeChisel extends Module {
+}
---
+++ src/main/scala/jpeg/quant.scala
@@ -0,0 +1,5 @@
+package jpeg
+
+import chisel3._
+import chisel3.internal.firrtl.Width
+import chisel3.util._
