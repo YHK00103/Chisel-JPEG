ffc8d09a4449e90419a4f6f836b9fb89f2b39dc1
Darren Lin <dlin52@ucsc.edu>
2024-03-01 20:34:14 -0800

    needs testing

--- src/main/scala/jpeg/quant.scala
+++ src/main/scala/jpeg/quant.scala
@@ -1,5 +1,20 @@
 package jpeg
 
 import chisel3._
-import chisel3.internal.firrtl.Width
 import chisel3.util._
+
+object Quantization {
+  def quantization(data: Vec[Vec[SInt]], quantTable: Vec[Vec[SInt]]): Vec[Vec[SInt]] = {
+    val result = Wire(Vec(8, Vec(8, SInt(data(0)(0).getWidth.W))))
+
+    for (i <- 0 until 8) {
+      for (j <- 0 until 8) { 
+        val resultValue = data(i)(j) / quantTable(i)(j)
+        val rounded = (resultValue + (resultValue >= 0.S).asSInt) / 2.S
+        result(i)(j) := rounded
+      }
+    }
+
+    result
+  }
+}
--- src/test/scala/jpeg/quantTester.scala
+++ src/test/scala/jpeg/quantTester.scala
@@ -1,6 +1,14 @@
 package jpeg
 
 import chisel3._
+import chisel3.util._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
-import java.beans.beancontext.BeanContextChildSupport
+
+class QuantizationTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "Quantization"
+
+  it should "correctly quantize data" in {
+    
+  }
+}
