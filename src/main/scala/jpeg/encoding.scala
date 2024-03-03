f8c82e692dbb9f7bdb5875a5977bdeb81ccc144b
Darren Lin <dlin52@ucsc.edu>
2024-03-02 17:40:38 -0800

    testing

--- src/main/scala/jpeg/encoding.scala
+++ src/main/scala/jpeg/encoding.scala
@@ -3,36 +3,95 @@ package jpeg
 import chisel3._
 import chisel3.util._
 
-object RLE {
-    def apply(data: Vec[SInt], result: Vec[SInt]) = {
-        val mod = Module(new RLE)
-        mod.io.data := data
-        result := mod.io.result
-        mod
-    }
+// object RLE {
+//     def apply(data: Vec[SInt], result: Vec[SInt]) = {
+//         val mod = Module(new RLE)
+//         mod.io.data := data
+//         result := mod.io.result
+//         mod
+//     }
+// }
+
+// class RLE extends Module {
+//     val io = IO(new Bundle{
+//         val data = Input(Vec(64, SInt(8.W)))
+//         val result = Output(Vec(64, SInt(8.W)))
+//     })
+
+// }
+
+object DeltaState extends ChiselEnum {
+    val idle, encode = Value
 }
 
-class RLE extends Module {
-    val io = IO(new Bundle{
-        val data = Input(Vec(64, SInt(8.W)))
-        val result = Output(Vec(64, SInt(8.W)))
+class Delta extends Module{
+    val io = IO(new Bundle {
+        val in = Flipped(Decoupled(new Bundle{
+            val data = Vec(64, SInt(8.W))
+        }))
+        val out = Valid(Vec(64, SInt(8.W)))
+        val state = Output(DeltaState())
     })
+    
+    val stateReg = RegInit(DeltaState.idle)
+    
+    val dataReg = RegInit(VecInit(Seq.fill(64)(0.S(8.W)))) // Initialize to all zeros
+    val outputReg = RegInit(VecInit(Seq.fill(64)(0.S(8.W)))) // Initialize output register
+    val dataIndex = RegInit(1.U(log2Ceil(64+1).W))
 
-}
+    io.state := stateReg
+    io.out.valid := false.B
+    io.out.bits := outputReg
+    io.in.ready := stateReg === DeltaState.idle
 
-object Delta {
-    def apply(data: Vec[SInt], result: Vec[SInt]) = {
-        val mod = Module(new Delta)
-        mod.io.data := data
-        result := mod.io.result
-        mod
+    switch(stateReg){
+        is(DeltaState.idle){
+            when(io.in.fire){
+                dataReg := io.in.bits.data
+                stateReg := DeltaState.encode
+            }
+        }
+
+        is(DeltaState.encode){
+            outputReg(0) := dataReg(0)
+            val diff = dataReg(dataIndex) - dataReg(dataIndex - 1.U)
+            outputReg(dataIndex) := diff
+            when (dataIndex < 64.U) {
+                dataIndex := dataIndex + 1.U
+            }
+            .otherwise{
+                io.out.valid := true.B
+                stateReg := DeltaState.idle
+            }
+        }
     }
-}
-
-class Delta extends Module {
-    val io = IO(new Bundle{
-        val data = Input(Vec(64, SInt(8.W)))
-        val result = Output(Vec(64, SInt(8.W)))
-    })
 
 }
+
+// object Delta {
+//     def apply(data: Vec[SInt], result: Vec[SInt]) = {
+//         val mod = Module(new Delta)
+//         mod.io.data := data
+//         result := mod.io.result
+//         mod
+//     }
+// }
+// class Delta extends Module {
+//     val io = IO(new Bundle{
+//         val data = Input(Vec(64, SInt(8.W)))
+//         val result = Output(Vec(64, SInt(8.W)))
+//     })
+
+//     val result = Wire(Vec(64, SInt(8.W)))
+//     val prev = RegInit(0.S(8.W))
+
+//     result(0) := io.data.head
+//     for (i <- 1 until 64) {
+//         val diff = io.data(i) - prev
+//         result(i) := diff
+//         prev := io.data(i)
+//     }
+
+//     io.result := result
+
+// }
--- src/test/scala/jpeg/encodingTester.scala
+++ src/test/scala/jpeg/encodingTester.scala
@@ -1,6 +1,36 @@
 package jpeg
 
 import chisel3._
+import chisel3.util._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
-import java.beans.beancontext.BeanContextChildSupport
+
+class DeltaTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "Delta Encoding"
+
+  it should "correctly do delta encoding" in {
+    test(new Delta).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+        dut.io.in.valid.poke(true.B)
+        dut.io.in.ready.expect(true.B)
+        dut.io.state.expect(DeltaState.idle)
+        val rand = scala.util.Random
+
+        // Iterate through each element of the Vec
+        for (i <- 0 until 64) {
+            // Generate a random SInt value
+            val randomSInt = rand.nextInt(64).S // Change 256 to your desired maximum value
+
+            // Poke the random SInt into io.in.bits(i)
+            dut.io.in.bits.data(i).poke(randomSInt)
+        }
+        // Run the clock
+        dut.clock.step()
+
+        dut.io.state.expect(DeltaState.encode)
+        dut.io.in.ready.expect(false.B)
+        dut.clock.step(64)
+        dut.io.state.expect(DeltaState.idle)
+
+    }
+  }
+}
