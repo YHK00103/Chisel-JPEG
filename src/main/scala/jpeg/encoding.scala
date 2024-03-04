dabe11c0cdc1a92c390b93fbc2d14286d7beca94
Darren Lin <dlin52@ucsc.edu>
2024-03-03 18:26:34 -0800

    more tests for Delta and failing 1-64 for RLE

--- src/main/scala/jpeg/encoding.scala
+++ src/main/scala/jpeg/encoding.scala
@@ -4,7 +4,7 @@ import chisel3._
 import chisel3.util._
 
 
-object RLEState extends ChiselEnum {
+object EncodingState extends ChiselEnum {
     val idle, encode = Value
 }
 
@@ -15,10 +15,10 @@ class RLE extends Module{
         }))
         val out = Valid(Vec(64, SInt(8.W)))
         val length = Valid(SInt())
-        val state = Output(RLEState())
+        val state = Output(EncodingState())
     })
     
-    val stateReg = RegInit(RLEState.idle)
+    val stateReg = RegInit(EncodingState.idle)
 
     // Initialize to all zeros
     val dataReg = RegInit(VecInit(Seq.fill(64)(0.S(8.W))))
@@ -26,9 +26,6 @@ class RLE extends Module{
     // Initialize output register
     val outputReg = RegInit(VecInit(Seq.fill(64)(0.S(8.W)))) 
     val dataIndex = RegInit(1.U(log2Ceil(64+1).W))
-
-    // val consecutiveReg = RegInit(VecInit(Seq.fill(64)(0.S(8.W))))
-    // val valueReg = RegInit(VecInit(Seq.fill(64)(0.S(8.W))))
 
     val consecutiveCounter = RegInit(0.U(log2Ceil(64+1).W))
     val valueCounter = RegInit(1.U(log2Ceil(64+1).W))
@@ -46,19 +43,18 @@ class RLE extends Module{
     io.length.bits := lenCounter
 
     io.state := stateReg
-    io.in.ready := stateReg === RLEState.idle
+    io.in.ready := stateReg === EncodingState.idle
 
     switch(stateReg){
-        is(RLEState.idle){
+        is(EncodingState.idle){
             when(io.in.fire){
                 current := io.in.bits.data(0)
-                // valueReg(0) := io.in.bits.data(0)
                 dataReg := io.in.bits.data
-                stateReg := RLEState.encode
+                stateReg := EncodingState.encode
             }
         }
 
-        is(RLEState.encode){
+        is(EncodingState.encode){
             when (dataIndex < 64.U) {
                 when(dataReg(dataIndex) === current){
                     consecutive := consecutive + 1.S
@@ -80,17 +76,10 @@ class RLE extends Module{
                 lenCounter := lenCounter + 2.S
                 io.out.valid := true.B
                 io.length.valid := true.B
-                stateReg := RLEState.idle
+                stateReg := EncodingState.idle
             }
         }
     }
-}
-
-
-
-
-object DeltaState extends ChiselEnum {
-    val idle, encode = Value
 }
 
 class Delta extends Module{
@@ -99,10 +88,10 @@ class Delta extends Module{
             val data = Vec(64, SInt(8.W))
         }))
         val out = Valid(Vec(64, SInt(8.W)))
-        val state = Output(DeltaState())
+        val state = Output(EncodingState())
     })
     
-    val stateReg = RegInit(DeltaState.idle)
+    val stateReg = RegInit(EncodingState.idle)
 
     // Initialize to all zeros
     val dataReg = RegInit(VecInit(Seq.fill(64)(0.S(8.W))))
@@ -114,17 +103,17 @@ class Delta extends Module{
     io.state := stateReg
     io.out.valid := false.B
     io.out.bits := outputReg
-    io.in.ready := stateReg === DeltaState.idle
+    io.in.ready := stateReg === EncodingState.idle
 
     switch(stateReg){
-        is(DeltaState.idle){
+        is(EncodingState.idle){
             when(io.in.fire){
                 dataReg := io.in.bits.data
-                stateReg := DeltaState.encode
+                stateReg := EncodingState.encode
             }
         }
 
-        is(DeltaState.encode){
+        is(EncodingState.encode){
             outputReg(0) := dataReg(0)
             when (dataIndex < 64.U) {
                 val diff = dataReg(dataIndex) - dataReg(dataIndex - 1.U)
@@ -133,7 +122,7 @@ class Delta extends Module{
             }
             .otherwise{
                 io.out.valid := true.B
-                stateReg := DeltaState.idle
+                stateReg := EncodingState.idle
             }
         }
     }
--- src/test/scala/jpeg/encodingTester.scala
+++ src/test/scala/jpeg/encodingTester.scala
@@ -11,12 +11,12 @@ class RLETest extends AnyFlatSpec with ChiselScalatestTester {
         test(new RLE).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
             dut.io.in.valid.poke(true.B)
             dut.io.in.ready.expect(true.B)
-            dut.io.state.expect(RLEState.idle)
+            dut.io.state.expect(EncodingState.idle)
             for (i <- 0 until 64) {
                 dut.io.in.bits.data(i).poke(data(i).S)
             }
             dut.clock.step()
-            dut.io.state.expect(RLEState.encode)
+            dut.io.state.expect(EncodingState.encode)
             dut.io.in.ready.expect(false.B)
             dut.clock.step(64)
             
@@ -26,7 +26,7 @@ class RLETest extends AnyFlatSpec with ChiselScalatestTester {
             for(i <- 0 until expected.length){
                 dut.io.out.bits(i).expect(expected(i).S)
             }
-            dut.io.state.expect(RLEState.idle)
+            dut.io.state.expect(EncodingState.idle)
 
             // // Printing each element of the array
             // val bitsArray: Vec[SInt] = dut.io.out.bits
@@ -37,8 +37,32 @@ class RLETest extends AnyFlatSpec with ChiselScalatestTester {
     }
 
     behavior of "RLEChisel"
-    it should "correctly encode (1, 3, 6, 10)" in {
-        val test = Seq(5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 11, 11, 11, 12, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14, 15, 15, 15, 16, 16, 16, 16, 16, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 19, 19, 19, 19, 19, 20, 20, 20)
+    it should "encode 5:3, 6:2, 7:5, 8:4, 9:3, 10:2, 11:3, 12:5, 13:4, 14:4, 15:3, 16:5, 17:6, 18:7, 19:5, 20:3" in {
+        val test = Seq(
+            5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 11, 
+            11, 11, 12, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14, 15, 
+            15, 15, 16, 16, 16, 16, 16, 17, 17, 17, 17, 17, 17, 18, 18, 18, 
+            18, 18, 18, 18, 19, 19, 19, 19, 19, 20, 20, 20
+        )
+        doRLETest(test)
+    }
+
+    it should "encode 1:8, 2:8, 3:8, 4:8, 5:8, 6:8, 7:8, 8:8" in {
+        val test = Seq(
+            1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 
+            3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 
+            6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8
+        )
+        doRLETest(test)
+    }
+
+    it should "encode no dupes" in {
+        val test = Seq(
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 
+            37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 
+            54, 55, 56, 57, 58, 59, 60, 61, 62, 62, 63, 64
+        )
         doRLETest(test)
     }
 
@@ -49,13 +73,13 @@ class DeltaTest extends AnyFlatSpec with ChiselScalatestTester {
         test(new Delta).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
             dut.io.in.valid.poke(true.B)
             dut.io.in.ready.expect(true.B)
-            dut.io.state.expect(DeltaState.idle)
+            dut.io.state.expect(EncodingState.idle)
             for (i <- 0 until 64) {
                 dut.io.in.bits.data(i).poke(data(i).S)
             }
             dut.clock.step()
 
-            dut.io.state.expect(DeltaState.encode)
+            dut.io.state.expect(EncodingState.encode)
             dut.io.in.ready.expect(false.B)
             dut.clock.step(64)
             val jpegEncoder = new jpegEncode(false, List.empty, 0)
@@ -63,15 +87,35 @@ class DeltaTest extends AnyFlatSpec with ChiselScalatestTester {
             for( i <- 0 until 64){
                 dut.io.out.bits(i).expect(expected(i).S)
             }
-            dut.io.state.expect(DeltaState.idle)
+            dut.io.state.expect(EncodingState.idle)
 
         }
     }
 
     behavior of "DeltaChisel"
-    it should "correctly encode (1, 3, 6, 10)" in {
-        val test = Seq(58, 21, 5, 3, 19, 27, 47, 34, 44, 56, 63, 61, 26, 59, 8, 51, 11, 12, 62, 14, 25, 16, 64, 22, 60, 33, 29, 2, 45, 54, 28, 32, 43, 6, 57, 46, 48, 9, 39, 10, 41, 50, 4, 30, 49, 37, 36, 55, 13, 7, 52, 53, 15, 31, 23, 38, 35, 18, 40, 42, 1, 24, 17, 20)
+    it should "encode 1 to 64" in {
+        val test = Seq(
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 
+            19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 
+            35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+            51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 62, 63, 64
+        )
+        doDeltaTest(test)
+    }
+    it should "encode 64 to 1" in {
+        val test = Seq(
+            64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49,
+            48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33,
+            32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17,
+            16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+        )
         doDeltaTest(test)
     }
 
+    it should "encode 64 of the same int(10 and 0)" in {
+        val test = Seq.fill(64)(10)
+        doDeltaTest(test)
+        val test = Seq.fill(64)(0)
+        doDeltaTest(test)
+    }
 }
