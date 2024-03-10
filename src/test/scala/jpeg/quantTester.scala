7bca7fca1fd647e2c44ed6dc27f0caabbab9cd1c
Darren Lin <dlin52@ucsc.edu>
2024-03-09 18:45:00 -0800

    changes to model tests

--- src/test/scala/jpeg/quantTester.scala
+++ src/test/scala/jpeg/quantTester.scala
@@ -90,17 +90,9 @@ class QuantizationTest extends AnyFlatSpec with ChiselScalatestTester {
     }
 
     it should "correctly quantize in3 with qt1" in {
-        // val data = jpeg.QuantizationData.in3
-        val in3 = Seq(Seq(120, -40, 80, 60, 90, -50, -30, 45),
-                  Seq(10, -30, 45, 25, 35, 15, -20, 30),
-                  Seq(-25, 35, 55, -15, 20, 40, 50, -10),
-                  Seq(5, -20, 30, 40, 26, 10, -15, 20),
-                  Seq(-10, 15, 20, 10, -5, 25, 30, -10),
-                  Seq(60, -30, -35, 20, 10, 15, -25, 35),
-                  Seq(30, -25, 40, 15, -10, 20, 25, -20),
-                  Seq(25, 15, -10, -20, 30, 45, -35, -15))
+        val data = jpeg.QuantizationData.in3
         val quantTable = jpeg.QuantizationTables.qt1
-        doQuantizationTest(in3, quantTable)
+        doQuantizationTest(data, quantTable)
     }
 
     it should "correctly quantize in3 with qt2" in {
